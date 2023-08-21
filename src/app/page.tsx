101d9e97347075be2e07c309e35d19c8df8f2d6c
hongduccodedao <contact.hongduc@gmail.com>
2023-08-21 15:08:35 +0700

    ✨ Add Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const Home = () => {
                 <div className="my-10">
                     <Banner />
                 </div>
-                <div className="grid grid-cols-4 gap-5 container mx-auto px-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 container mx-auto px-8">
                     <Card font={font} />
                     <Card font={font} />
                     <Card font={font} />
